f0fc4f41d42c2893f5955507e164836286d31763
Harshit Jaiswal <Hjaiswal02@gmail.com>
2020-05-10 16:36:08 +0530

    Build models and connect to sqlite DB

--- 5.Django/first_project/first_app/urls.py
+++ 5.Django/first_project/first_app/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from django.urls import path
 urlpatterns = [
     path('',views.index,name='index'),
-    path('help',views.helpme,name='help')
+    path('help/',views.helpme,name='help')
 ]
--- 5.Django/first_project/first_app/views.py
+++ 5.Django/first_project/first_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from first_app.models import  Topic, Webpage, AccessRecord
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django")
+    webpages_list = AccessRecord.objects.order_by('date')
+    data_dict = {'access_records':webpages_list}
+    return render(request, 'first_app/startup_page.html', context=data_dict)
 
 def  helpme(request):
-    helpDic = {
+
+    help_dic = {
         'details' : 'Welcome to our help Doc !'
     }
-    return render(request,'first_app/index.html',context=helpDic)
+    return render(request, 'first_app/index.html', context=help_dic)
