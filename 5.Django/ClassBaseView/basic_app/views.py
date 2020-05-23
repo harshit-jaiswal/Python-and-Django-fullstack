2e8ccea734a9013a4ac979a182f5323c053d9052
Harshit Jaiswal <Hjaiswal02@gmail.com>
2020-05-23 15:56:31 +0530

    ClassBaseView and CRUD Views

---
+++ 5.Django/ClassBaseView/basic_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.views.generic import View,TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
+from . import models
+from django.urls import reverse_lazy
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECYION!'
+        return context
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+    # school_list 
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+    
+class SchoolCreateView(CreateView):
+    model = models.School
+    fields =('name','principal','location')
+    
+class SchoolUpdateView(UpdateView):
+    fields = ('name','principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy("basic_app:list")
